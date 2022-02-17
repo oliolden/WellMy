cfd9a3d837bb73cfc349fcbe5ca680b5aa71cc37
oliolden <oliver@oldenstam.se>
2022-02-17 13:14:13 +0100

    More namespace changes.

--- PI_App/App.xaml.cs
+++ PI_App/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace PI_App {
+namespace WellMy {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
