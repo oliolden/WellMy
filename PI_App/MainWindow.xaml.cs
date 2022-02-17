e69f23e8823a3bb5ebb7b23d4179ac8be5bbca77
oliolden <oliver@oldenstam.se>
2022-02-17 13:07:56 +0100

    Changed Namespace and cleaned up old code.

--- PI_App/MainWindow.xaml.cs
+++ PI_App/MainWindow.xaml.cs
@@ -14,34 +14,13 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Threading;
 
-namespace PI_App {
+namespace WellMy {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-
-    class Data {
-        public static List<double> data = new List<double>();
-        public static List<Thread> threads = new List<Thread>();
-    }
-
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
-            while (true) {
-                Data.threads.Add(new Thread(ThreadProc));
-                Data.threads.Last().Start();
-            }
-        }
-
-        public void ThreadProc(object stateInfo) {
-            Random random = new Random();
-            while (true) {
-                Data.data.Add(Calc(Calc(random.NextDouble(), random.NextDouble()), Calc(random.NextDouble(), random.NextDouble())));
-            }
-        }
-
-        private double Calc(double x, double y) {
-            return Math.Sin(x % y);
         }
     }
 }
